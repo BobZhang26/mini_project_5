3f985366cc571f24d549a9f05973506d6304ab07
Bobby Zhang <z.zhang19@imperial.ac.uk>
2023-10-23 14:07:48 -0400

    path

--- mylib/extract.py
+++ mylib/extract.py
@@ -2,11 +2,11 @@
 extract a dataset from URL
 """
 import requests
-
+import os
 
 def extract(
     url="https://raw.githubusercontent.com/MainakRepositor/Datasets/master/Chennai%20rain/chennai_reservoir_rainfall.csv",
-    filepath="../data/rainfall.csv",
+    filepath="./data/rainfall.csv",
 ):
     with requests.get(url,timeout=5) as r:
         with open(filepath, "wb") as f:
@@ -15,4 +15,9 @@ def extract(
 
 
 if __name__ == "__main__":
+    if os.path.exists('/Users/zhangbobby/Desktop/IDS706/mini_project_5'):
+        os.chdir('/Users/zhangbobby/Desktop/IDS706/mini_project_5')
+    else:
+        print("Directory does not exist.")
+
     extract()
