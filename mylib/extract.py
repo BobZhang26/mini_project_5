836246a0482d8dcb877bc06a52ea2a50b1d85f6b
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-10-25 00:21:26 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -6,7 +6,7 @@ import os
 
 def extract(
     url="https://raw.githubusercontent.com/MainakRepositor/Datasets/master/Chennai%20rain/chennai_reservoir_rainfall.csv",
-    filepath="./data/rainfall.csv",
+    filepath="rainfall.csv",
 ):
     with requests.get(url,timeout=5) as r:
         with open(filepath, "wb") as f:
